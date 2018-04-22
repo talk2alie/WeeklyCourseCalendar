5eea66c3fbc6447a6e3002a59b605679cd79f055
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2018-04-22 17:40:32 -0400

    Add constructor test for WeeklySchedule

---
+++ WeeklyCourseCalendar.Domain.Tests/WeeklyScheduleTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace WeeklyCourseCalendar.Domain.Tests
+{
+    public class WeeklyScheduleTests
+    {
+        [Fact, Trait("Category", "WeeklySchedule")]
+        public void WeeklySchedule_CanCreateInstance()
+        {
+            // Arrange
+
+            // Act
+            var weeklySchedule = new WeeklySchedule();
+
+            // Assert
+            Assert.NotNull(weeklySchedule);
+        }
+    }
+}
