fe17c81708e30d8a819a5361a4904292e495067e
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2018-04-22 17:40:08 -0400

    Add WeeklySchedule constructor

--- WeeklyCourseCalendar.Domain/WeeklySchedule.cs
+++ WeeklyCourseCalendar.Domain/WeeklySchedule.cs
@@ -1,13 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WeeklyCourseCalendar.Domain
 {
     public class WeeklySchedule
     {
+        private readonly Dictionary<DaysOfWeek, HashSet<TimeSlot>> _availableSlotsPerDay;
+        private readonly List<DaysOfWeek> _schoolDays;
+
         public string SemesterName { get; set; }
 
         public DateTime SemesterStartDate { get; set; }
 
         public DateTime SemesterEndDate { get; set; }
+
+        public WeeklySchedule()
+        {
+            _schoolDays = new List<DaysOfWeek>
+            {
+                DaysOfWeek.Monday,
+                DaysOfWeek.Tuesday,
+                DaysOfWeek.Wednesday,
+                DaysOfWeek.Thursday,
+                DaysOfWeek.Friday
+            };
+            _availableSlotsPerDay = new Dictionary<DaysOfWeek, HashSet<TimeSlot>>();
+            CreateSlotsForSchoolDays();
+        }
+
+        private void CreateSlotsForSchoolDays()
+        {
+            var schoolStartTime = DateTime.Parse("8:00 AM");
+            var schoolEndTime = DateTime.Parse("9:00 PM");
+            const int slotDuration = 5;
+
+            _schoolDays.ForEach(schoolDay =>
+            {
+                var timeSlotsForDay = new HashSet<TimeSlot>();
+                DateTime currentTime = schoolStartTime;
+                while (currentTime.TimeOfDay <= schoolEndTime.TimeOfDay)
+                {
+                    timeSlotsForDay.Add(new TimeSlot(day: schoolDay, time: currentTime));
+                    currentTime = currentTime.AddMinutes(slotDuration);
+                }
+                _availableSlotsPerDay.Add(schoolDay, timeSlotsForDay);
+            });
+        }
     }
 }
