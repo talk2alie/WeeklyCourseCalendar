e98e33ba9ea5e0037ebaa1cb90560214a3193732
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2018-04-25 18:05:06 -0400

    Add project for the data layer of the application

---
+++ WeeklyCourseCalendar.Data/Course.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeeklyCourseCalendar.Data
+{
+    public class Course
+    {
+        public string Number { get; set; }
+
+        public string Name { get; set; }
+
+        public string Section { get; set; }
+
+        public string Instructor { get; set; }
+
+        public string Comment { get; set; }
+
+        public string Attributes { get; set; }
+
+        public List<Schedule> Schedules { get; set; } = new List<Schedule>();
+
+        public override string ToString()
+        {
+            string courseText = $"{Number} - {Section} {Name}: ";
+            Schedules.ForEach(schedule => courseText += schedule + "; ");
+            return courseText.Substring(0, courseText.Length - 2);
+        }
+    }
+}
---
+++ WeeklyCourseCalendar.Data/Schedule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WeeklyCourseCalendar.Data
+{
+    public class Schedule
+    {
+        public DaysOfWeek Days { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Days} from {StartTime.ToShortTimeString()} to {EndTime.ToShortTimeString()}";
+        }
+    }
+}
